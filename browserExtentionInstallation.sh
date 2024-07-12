8d0fd102d95f3fd9d178dbdbe3bae454439c0042
Pranay Chatterjee <75167085+pranay-op@users.noreply.github.com>
2024-07-12 09:45:22 +0530

    Browser Extension Installation
    This script will enable Bug Hunters, to get along with their Testing, and spend less time setting up the environment.

---
+++ browserExtentionInstallation.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Define variables for the extension URL and its name
+EXTENSION_URL="https://addons.mozilla.org/firefox/downloads/file/4316416/wappalyzer-6.10.70.xpi"
+EXTENSION_NAME="Wappalyzer"
+
+# Open Firefox and navigate to the extension URL
+firefox "$EXTENSION_URL"
+
+# Wait for the user to manually add the extension (can be adjusted based on the extension's installation process)
+read -p "Please add the extension '$EXTENSION_NAME' manually and press Enter when done..."
+
+# Optionally, you can add additional commands here to automate further configuration or testing of the extension
+# For example, refreshing the browser or opening a specific webpage to test the extension
+
+echo "Extension installation and configuration completed."
+
+EXTENSION_URL="https://addons.mozilla.org/firefox/downloads/file/3716439/laboratory_by_mozilla-3.0.8.xpi"
+EXTENSION_NAME="Laboratory"
+
+# Open Firefox and navigate to the extension URL
+firefox "$EXTENSION_URL"
+
+# Wait for the user to manually add the extension (can be adjusted based on the extension's installation process)
+read -p "Please add the extension '$EXTENSION_NAME' manually and press Enter when done..."
+
+# Optionally, you can add additional commands here to automate further configuration or testing of the extension
+# For example, refreshing the browser or opening a specific webpage to test the extension
+
+echo "Extension installation and configuration completed."
+
+
+EXTENSION_URL="https://addons.mozilla.org/firefox/downloads/file/4228676/foxyproxy_standard-8.9.xpi"
+EXTENSION_NAME="Foxy Proxy"
+
+# Open Firefox and navigate to the extension URL
+firefox "$EXTENSION_URL"
+
+# Wait for the user to manually add the extension (can be adjusted based on the extension's installation process)
+read -p "Please add the extension '$EXTENSION_NAME' manually and press Enter when done..."
+
+# Optionally, you can add additional commands here to automate further configuration or testing of the extension
+# For example, refreshing the browser or opening a specific webpage to test the extension
+
+echo "Extension installation and configuration completed."
+
+
+EXTENSION_URL="https://addons.mozilla.org/firefox/downloads/file/4117305/shodan_addon-1.1.1.xpi"
+EXTENSION_NAME="Shodan"
+
+# Open Firefox and navigate to the extension URL
+firefox "$EXTENSION_URL"
+
+# Wait for the user to manually add the extension (can be adjusted based on the extension's installation process)
+read -p "Please add the extension '$EXTENSION_NAME' manually and press Enter when done..."
+
+# Optionally, you can add additional commands here to automate further configuration or testing of the extension
+# For example, refreshing the browser or opening a specific webpage to test the extension
+
+echo "Extension installation and configuration completed."
+
+EXTENSION_URL="https://addons.mozilla.org/firefox/downloads/file/3384326/http_header_live-0.6.5.2.xpi"
+EXTENSION_NAME="HTTP Header Live"
+
+# Open Firefox and navigate to the extension URL
+firefox "$EXTENSION_URL"
+
+# Wait for the user to manually add the extension (can be adjusted based on the extension's installation process)
+read -p "Please add the extension '$EXTENSION_NAME' manually and press Enter when done..."
+
+# Optionally, you can add additional commands here to automate further configuration or testing of the extension
+# For example, refreshing the browser or opening a specific webpage to test the extension
+
+echo "Extension installation and configuration completed."
+
+EXTENSION_URL="https://addons.mozilla.org/firefox/downloads/file/4241002/cookie_editor-1.13.0.xpi"
+EXTENSION_NAME="Cookie Editor"
+
+
+# Open Firefox and navigate to the extension URL
+firefox "$EXTENSION_URL"
+
+# Wait for the user to manually add the extension (can be adjusted based on the extension's installation process)
+read -p "Please add the extension '$EXTENSION_NAME' manually and press Enter when done..."
+
+# Optionally, you can add additional commands here to automate further configuration or testing of the extension
+# For example, refreshing the browser or opening a specific webpage to test the extension
+
+echo "Extension installation and configuration completed."
